50b196e260ddd345c71c2aa30af9e88db290df5f
Zachary Morel <morelz133@gmail.com>
2017-10-05 13:57:15 -0400

    It's ready for usage with my app

--- 01-Authorization-RS256/server.js
+++ 01-Authorization-RS256/server.js
@@ -35,9 +35,9 @@ app.get('/api/public', function(req, res) {
   });
 });
 
-app.get('/api/private', checkJwt, checkScopes, function(req, res) {
+app.get('/api/private', checkJwt, function(req, res) {
   res.json({
-    message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
+    message: 'Hello from a private endpoint! You need to be authenticated.'
   });
 });
 
